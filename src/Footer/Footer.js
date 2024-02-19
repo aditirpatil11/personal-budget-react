88cde2a6f14fa8a2ac80d496af268b06ed7a5330
aditirpatil11 <aditirpatil11@gmail.com>
2024-02-18 23:20:50 -0500

    Brought PBCode In

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+    <div className="center">All rights reserved &copy; Tanvi Jonnada</div>
+  </footer>
+  );
+}
+
+export default Footer;
